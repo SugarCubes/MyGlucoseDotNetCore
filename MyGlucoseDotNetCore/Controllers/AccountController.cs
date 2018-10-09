9743c48a91a5f5b0e017aa910dc099cabab148b3
jtble <jt.blevins@gmail.com>
2018-10-09 16:55:58 -0400

    Updated the AccountController > RemoteLogin comments and documentation - Also made minor change to README file

--- MyGlucoseDotNetCore/Controllers/AccountController.cs
+++ MyGlucoseDotNetCore/Controllers/AccountController.cs
@@ -87,8 +87,11 @@ namespace MyGlucoseDotNetCore.Controllers
             return View( model );
         }
 
-
-
+        
+        /// <summary>Allows a patient to login from a remote device.</summary>
+        /// <param name="Email">The patient's email.</param>
+        /// <param name="Password">The patient's password.</param>
+        /// <returns>JsonResult</returns>
         [HttpPost]
         [AllowAnonymous]
         //[ValidateAntiForgeryToken]
@@ -96,21 +99,21 @@ namespace MyGlucoseDotNetCore.Controllers
         {
             if ( ModelState.IsValid )
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                // result is a boolean value: whether or not the signin was successful:
                 var result = await _signInManager.PasswordSignInAsync( Email, Password, true, lockoutOnFailure: false);
-                if ( result.Succeeded )
+                if ( result.Succeeded )                                     // Login was successful
                 {
-                    ApplicationUser user = await _users.ReadAsync( Email );//await _userManager.GetUserAsync( User );
-                    _logger.LogInformation( "User logged in remotely." );
+                    ApplicationUser user = await _users.ReadAsync( Email ); // Read user from the repository
+                    _logger.LogInformation( "User logged in remotely." );   // Probably not needed
 
-                    user.RemoteLoginToken = Guid.NewGuid();
+                    user.RemoteLoginToken = Guid.NewGuid();                 // Create a login token, similar to a "session id"
+                    // Not tested: Probably won't be used to force user to login again until much later in development:
                     user.RemoteLoginExpiration = (long) DateTime.UtcNow.Subtract( new DateTime( 1970, 1, 1 ).AddDays( 30 ) ).TotalSeconds;
-                    await _users.UpdateAsync( user.UserName, user );
+                    await _users.UpdateAsync( user.UserName, user );        // Update the user with the repo
 
-                    return new JsonResult(
-                        new
-                        {
+                    return new JsonResult(                                  // This implements IActionResult. If you were 
+                        new                                                 //      to inspect the output, you would see a 
+                        {                                                   //      Json-formatted string.
                             success = true,
                             errorCode = ErrorCode.NO_ERROR,
                             user.UserName,
@@ -129,22 +132,32 @@ namespace MyGlucoseDotNetCore.Controllers
                             user.Zip2
                         }
                         );
+
                 }
-                else
+                else                                                            // Login didn't work
                 {
-                    return new JsonResult(
+                    return new JsonResult(                                      // We still return a JsonResult
                             new
                             {
-                                success = false,
-                                errorCode = ErrorCode.INVALID_EMAIL_PASSWORD
+                                success = false,                                // Indicate the login was unsuccessful
+                                errorCode = ErrorCode.INVALID_EMAIL_PASSWORD    //  ...and return an error code
                             }
                         );
-                }
-            }
 
-            // If we got this far, something failed, redisplay form
-            return new JsonResult( new { success = false } );
-        }
+                } // if succeeeded...else
+
+            } // ModelState == valid
+
+            // If we got this far, something failed, so just return an unknown error
+            return new JsonResult( 
+                    new
+                    {
+                        success = false,
+                        errorCode = ErrorCode.UNKNOWN
+                    }
+                );
+
+        } // LoginRemote
 
 
         [HttpGet]
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyGlucoseDotNetCore
+# MyGlucose (.Net Core doctor application)
 The doctor's view/backend website for the MyGlucose application
 
 To use the web server during testing, you will need to configure IIS Express to allow external access (from mobile devices or other computers):
@@ -23,4 +23,4 @@ To use the web server during testing, you will need to configure IIS Express to 
 </site>
 ```
    - Remember to leave the first 2 lines so that testing will still work in your browser
-   - !!!NOTE!!! You **_must_** start VS with Administrator rights so that IIS will bind the wildcard addresses. Otherwise, it will give an error message _"The hostname could not be parsed"_
+   - **!!!NOTE!!! You _MUST_ start Visual Studio with Administrator rights so that IIS will bind the wildcard addresses.** Otherwise, it will give an error message _"The hostname could not be parsed"_
