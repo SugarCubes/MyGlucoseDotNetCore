e38d88fb592ea950cf879169203103f4fcfec401
Austin Bowman <bowmanat@etsu.edu>
2018-12-03 18:41:46 -0500

    Doctor Date Range Selection - to be tested

--- MyGlucoseDotNetCore/Areas/API/Controllers/ChartApiController.cs
+++ MyGlucoseDotNetCore/Areas/API/Controllers/ChartApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyGlucoseDotNetCore.Models;
 using MyGlucoseDotNetCore.Services.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -23,12 +24,20 @@ namespace MyGlucoseDotNetCore.Areas.API.Controllers
 
         } // constructor
 
-        public JsonResult GetUserExerciseChart(string UserName)
+        public JsonResult GetUserExerciseChart(string UserName, DateTime? fromDate = null, DateTime? toDate = null)
         {
-            var data = _excerciseEntryRepo
+            IQueryable<ExerciseEntry> data = _excerciseEntryRepo
                 .ReadAll()
                 .Where(e => e.UserName == UserName && e.Minutes > 0 )
                 .OrderBy(e => e.UpdatedAt);
+            if (fromDate != null)
+                data = data.Where(d => d.UpdatedAt >= fromDate);
+            if (toDate != null)
+            {
+                DateTime addADay = (DateTime)toDate;
+                addADay = addADay.AddHours(23).AddMinutes(59);
+                data = data.Where(d => d.UpdatedAt <= addADay);
+            }
             return new JsonResult(new { exerciseEntries = data });
 
         } // GetGlucoseChart
@@ -44,24 +53,40 @@ namespace MyGlucoseDotNetCore.Areas.API.Controllers
         } // GetGlucoseChart
 
 
-        public JsonResult GetUserGlucoseChart(string UserName)
+        public JsonResult GetUserGlucoseChart(string UserName, DateTime? fromDate = null, DateTime? toDate = null)
         {
-            var data = _glucoseEntryRepo
+            IQueryable<GlucoseEntry> data = _glucoseEntryRepo
                 .ReadAll()
                 .Where(o => o.UserName == UserName)
                 .OrderBy(o => o.UpdatedAt);
+            if (fromDate != null)
+                data = data.Where(d => d.UpdatedAt >= fromDate);
+            if (toDate != null)
+            {
+                DateTime addADay = (DateTime)toDate;
+                addADay = addADay.AddHours(23).AddMinutes(59);
+                data = data.Where(d => d.UpdatedAt <= addADay);
+            }
             return new JsonResult(new { glucoseEntries = data });
 
         } // GetGlucoseChart
 
 
-        public JsonResult GetUserMealChart( string UserName )
+        public JsonResult GetUserMealChart(string UserName, DateTime? fromDate = null, DateTime? toDate = null)
         {
             // TODO: Create ReadAll(Username)
-            var data = _mealEntryRepo
+            IQueryable<MealEntry> data = _mealEntryRepo
                 .ReadAll()
                 .Where(o => o.UserName == UserName)
                 .OrderBy(o => o.UpdatedAt);
+            if (fromDate != null)
+                data = data.Where(d => d.UpdatedAt >= fromDate);
+            if (toDate != null)
+            {
+                DateTime addADay = (DateTime)toDate;
+                addADay = addADay.AddHours(23).AddMinutes(59);
+                data = data.Where(d => d.UpdatedAt <= addADay);
+            }
             return new JsonResult( new { mealEntries = data } );
 
         } // GetMealChart
--- MyGlucoseDotNetCore/wwwroot/js/exerciseChart.js
+++ MyGlucoseDotNetCore/wwwroot/js/exerciseChart.js
@@ -1,11 +1,37 @@
 ﻿$(document).ready(function () {
 
+    // From: https://stackoverflow.com/a/25359264
+    $.urlParam = function (name) {
+        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (results === null) {
+            return null;
+        }
+        return decodeURI(results[1]) || 0;
+    } // urlParam
+
     google.charts.load('current', { 'packages': ['line'] });
     google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
+    $(document).on('change', ".fromDate", function (incomingDate) {
+        //$url += "&fromDate=" + $(".fromDate").val();
+        google.charts.load('current', { 'packages': ['line'] });
+        google.charts.setOnLoadCallback(drawChart);
+    });
 
+    $(document).on('change', ".toDate", function (incomingDate) {
+        //alert($(".toDate").val());
+        //$url += "&toDate=" + $(".toDate").val();
+        google.charts.load('current', { 'packages': ['line'] });
+        google.charts.setOnLoadCallback(drawChart);
+    });
+
+    function drawChart() {
         $url = "/API/ChartApi/GetUserExerciseChart?UserName=" + $.urlParam("UserName");
+        if ($(".fromDate").val() != null && $(".fromDate").val() != "")
+            $url += "&fromDate=" + $(".fromDate").val();
+        if ($(".toDate").val() != null && $(".toDate").val() != "")
+            $url += "&toDate=" + $(".toDate").val();
+  
         console.log("Sending request: [href: " + $url + "]" + ";");
 
         $.ajax({
@@ -42,8 +68,7 @@
                 if (exerciseArray.length > 0)
                     chart.draw(gData, google.charts.Line.convertOptions(options));
 
-                //let $div = $('#feat_' + response.featureId);
-                //$div.remove();                              // Delete the row entirely
+                $url = null;
             },
             error: function (response) {
                 console.log(response);
@@ -52,15 +77,5 @@
         });
 
     } // drawChart
-
-
-    // From: https://stackoverflow.com/a/25359264
-    $.urlParam = function (name) {
-        var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-        if (results === null) {
-            return null;
-        }
-        return decodeURI(results[1]) || 0;
-    } // urlParam
 
 }); // document onLoad
