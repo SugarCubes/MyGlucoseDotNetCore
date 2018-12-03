fb73d412571a2ad297ca27a95d869066e41ec4f9
jtble <jt.blevins@gmail.com>
2018-12-03 18:26:35 -0500

    Updated for merge

--- MyGlucoseDotNetCore/Startup.cs
+++ MyGlucoseDotNetCore/Startup.cs
@@ -30,8 +30,11 @@ namespace MyGlucoseDotNetCore
             services.AddScoped<DatabaseSeeder>();
 
             services.AddDbContextPool<ApplicationDbContext>( options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+              options.UseMySql( Configuration.GetConnectionString( "MySqlConnection" ) ) );//,
+                    //mysqlOptions =>
+                    //{
+                    //    mysqlOptions.ServerVersion( new Version( 5, 5, 60 ), ServerType.MySql );
+                    //} ) );
 
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
@@ -64,7 +67,7 @@ namespace MyGlucoseDotNetCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure( IApplicationBuilder app, IHostingEnvironment env )
         {
-            if ( env.IsDevelopment() )
+            if( env.IsDevelopment() )
             {
                 app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
@@ -80,16 +83,16 @@ namespace MyGlucoseDotNetCore
             app.UseAuthentication();
 
             app.UseMvc( routes =>
-             {
-                 routes.MapRoute(
-                     name: "default",
-                     template: "{controller=Home}/{action=Index}/{id?}" );
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}" );
 
-                 routes.MapRoute(
-                     name: "areas",
-                     template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                 );
-             } );
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            } );
         }
     }
 }
